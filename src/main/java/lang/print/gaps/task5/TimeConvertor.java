cb0d4dc47dcd04eec6b26132c319cf11752856be
joel912 <joelhehehe1234@gmail.com>
2025-05-24 11:04:49 +0530

    Update TimeConvertor.java

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
-        int ans = minutes*60;
+        float ans = minutes*60;
         System.out.println(ans);
     }
 }
